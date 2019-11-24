10abd1d534f5287087530527a43e8251a8a7b537
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-11-24 15:29:56 +0000

    testing that an unregistered user can not log in

--- cypress/integration/feature_test_spec.js
+++ cypress/integration/feature_test_spec.js
@@ -1,23 +1,38 @@
 describe('Users can see an authorization link on the welcome page', function() {
-  it('There is a form for authorization on the landing page', function() {
-    cy.visit('http://localhost:3000')
-    cy.contains('Sign In')
-  })
+it('There is a form for authorization on the landing page', function() {
+  cy.visit('http://localhost:3000')
+  cy.contains('Sign In')
+})
 
-  // it('There is button for viewing bikes', function() {
-  //   cy.visit('http://localhost:3000')
-  //   cy.contains('Click here to view your Bikes')
-  // })
-  //
-  // it('Clicking view your Bikes redirects to a page with a Bikes element', function() {
-  //   cy.visit('http://localhost:3000')
-  //   cy.contains('Click here to view your Bikes').click()
-  //   cy.contains('Bikes')
-  // })
-  //
-  // it('Clicking the home button redirects to the home page', function() {
-  //   cy.visit('http://localhost:3000/bikes')
-  //   cy.contains('Home').click()
-  //   cy.url().should('include', 'http://localhost:3000')
-  // })
+it('An unregistered user can not sign in, an alert message is displayed', function() {
+  cy.visit('http://localhost:3000')
+  cy.get('#email').type('something@mail')
+  cy.get('#password').type('23dsf')
+  cy.get('form').submit()
+  cy.on('uncaught:exception', (err, runnable) => {
+  return false
+})
+  cy.on('window:alert', (str) => {
+  expect(str).to.equal(`Your login details are incorrect, please try again.`)
+})
+
+
+})
+
+// it('There is button for viewing bikes', function() {
+//   cy.visit('http://localhost:3000')
+//   cy.contains('Click here to view your Bikes')
+// })
+//
+// it('Clicking view your Bikes redirects to a page with a Bikes element', function() {
+//   cy.visit('http://localhost:3000')
+//   cy.contains('Click here to view your Bikes').click()
+//   cy.contains('Bikes')
+// })
+//
+// it('Clicking the home button redirects to the home page', function() {
+//   cy.visit('http://localhost:3000/bikes')
+//   cy.contains('Home').click()
+//   cy.url().should('include', 'http://localhost:3000')
+// })
 })
