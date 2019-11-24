fe5df885697ee1348f311a5aed9bced5220dcbb3
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-11-24 13:48:22 +0000

    added cypress test to verify that there is a sign up form on the home page

--- cypress/integration/feature_test_spec.js
+++ cypress/integration/feature_test_spec.js
@@ -1,23 +1,23 @@
 describe('Users can see an authorization link on the welcome page', function() {
-  it('There is a link for authorization', function() {
+  it('There is a form for authorization on the landing page', function() {
     cy.visit('http://localhost:3000')
-    cy.contains('HERE')
+    cy.contains('Sign In')
   })
 
-  it('There is button for viewing bikes', function() {
-    cy.visit('http://localhost:3000')
-    cy.contains('Click here to view your Bikes')
-  })
-
-  it('Clicking view your Bikes redirects to a page with a Bikes element', function() {
-    cy.visit('http://localhost:3000')
-    cy.contains('Click here to view your Bikes').click()
-    cy.contains('Bikes')
-  })
-
-  it('Clicking the home button redirects to the home page', function() {
-    cy.visit('http://localhost:3000/bikes')
-    cy.contains('Home').click()
-    cy.url().should('include', 'http://localhost:3000')
-  })
+  // it('There is button for viewing bikes', function() {
+  //   cy.visit('http://localhost:3000')
+  //   cy.contains('Click here to view your Bikes')
+  // })
+  //
+  // it('Clicking view your Bikes redirects to a page with a Bikes element', function() {
+  //   cy.visit('http://localhost:3000')
+  //   cy.contains('Click here to view your Bikes').click()
+  //   cy.contains('Bikes')
+  // })
+  //
+  // it('Clicking the home button redirects to the home page', function() {
+  //   cy.visit('http://localhost:3000/bikes')
+  //   cy.contains('Home').click()
+  //   cy.url().should('include', 'http://localhost:3000')
+  // })
 })
