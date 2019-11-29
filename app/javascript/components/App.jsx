bd2797b51c48a1ca8620c3849df8cd15a882149e
EdZeno <eduard.kulcsickij.gut@gmail.com>
2019-11-29 10:51:38 +0000

    refractor for linter

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Routes from "../routes/Index";
 
+/* eslint-disable no-alert, no-unused-vars */
 /* eslint-disable no-alert, react/display-name */
 export default props => <>{Routes}</>;
 /* eslint-enable no-alert, react/display-name */
+/* eslint-enable no-alert,  no-unused-vars*/
